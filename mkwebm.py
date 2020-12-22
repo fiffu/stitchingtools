d97e6e04ffd6ce921fb9888239be8960ac4e4644
fiffu <fhejehfif@gmail.com>
2020-12-23 01:33:25 +0800

    mkwebm: Looping overhaul; add /align; support fullwidth charsets

--- mkwebm.py
+++ mkwebm.py
@@ -1,45 +1,172 @@
-from enum import Enum
+from glob import glob
 import io
+from itertools import zip_longest
 import os
 from os.path import exists
 import re
-import shutil
 from subprocess import run, STDOUT, DEVNULL
-from types import SimpleNamespace
+from typing import Tuple
 
 from apng import APNG, PNG
 from PIL import Image  # pip install pillow
+
+# typedefs
+RgbTuple = Tuple[int, int, int]
 
 
 FPS = 7
 OUT_FORMAT = (
     # '.webm'
-    '.apng'
-    # '.gif'
+    # '.apng'
+    '.gif'
 )
 
 VERBOSITY = 1
 FORCE_WRITE_FRAMES = True
 
 OPTION_TYPES = {
-    'rescale': int
+    'rescale': float,
+    'charset': str,  # 'int' or 'ascii' or 'fullwidth'
+    'align': str, # [w|s|c]  (compass cardinals) (warning: slow!)
+    'bgcol': RgbTuple,  # specify '255,0,0' without spaces for #ff0000
 }
 
-
 text = """
-/rescale 3
+/bgcol 50,50,50
+/align w
 
-
-0, 白部屋 下, 1-1-1.1~4
-4, halt
+0, scene_effects_under1~3@, scene_2-1~8, scene_effects_above1~3@
+8, halt
 """
+
+
 
 class ParseError(ValueError):
     pass
 
-class RepeatBy(Enum):
-    LOOP = 1
-    PENDULUM = 2
+class Loopr:
+    class LoopType:
+        A = 'A'        # 0000
+        AB = 'AB'      # 0123
+        ABB = 'ABB'    # 0123333
+        ABAB = 'ABAB'  # 0123 0123
+        ABA = 'ABA'    # 0 123 210 123 210
+        ABBA = 'ABBA'  # 0123 3210 0123 3210
+
+    __slots__ = 'by val inverse start steps'.split()
+
+    def __init__(self, val, by: str = 'A', start=0, steps=-1):
+        self.by = by
+        self.val = val
+        self.inverse = False
+        self.start = start
+        self.steps = steps
+
+        if hasattr(self.LoopType, by):
+            pass
+        else:
+            ay = ''.join('AB'[x == 'A'] for x in by)
+            if hasattr(self.LoopType, ay):
+                self.inverse = True
+                self.val = val[::-1]
+                self.by = ay
+            else:
+                raise NotImplementedError(f'unsupported LoopType: {by}')
+
+    def __iter__(self):
+        return self.loop()
+
+    def _loop(self):
+        lty = self.LoopType  # shorthands
+
+        if self.by == lty.A:
+            while True:
+                yield self.val
+
+        if self.by == lty.AB:
+            yield from self.val
+            return
+
+        if self.by == lty.ABB:
+            yield from self.val
+            while True:
+                yield self.val[-1]
+
+        if self.by == lty.ABAB:
+            # if val=='0123' and start_idx==2, yield 23 0123 0123...
+            # if wanted 343434... should just pass val=='34'
+            while True:
+                yield from self.val
+
+        if self.by in [lty.ABA, lty.ABBA]:
+            val, nmax = self.val, (len(self.val) - 1)
+
+            yield self.val[0]
+
+            n, step = 1, +1
+
+            while True:
+                yield val[n]
+
+                # "Bounce" at start and end of iterable
+                if n == 0 or n == nmax:
+                    step = -step
+                    # For ABBA, yield the extra "A" or "B" before stepping n
+                    if self.by == lty.ABBA:
+                        yield val[0] if n == 0 else val[-1]
+                    else:
+                        first = False
+                n += step
+
+    def loop(self, start=None, steps=None):
+        start = start or self.start
+        steps = steps or self.steps
+        ctr = 0
+        for i, val in enumerate(self._loop()):
+            if i < start:
+                continue
+            if ctr == steps:
+                break
+            yield val
+            ctr += 1
+
+    def __repr__(self):
+        args = ', '.join(f'{k}={getattr(self, k)}' for k in self.__slots__)
+        return f'Loopr({repr(self.val)}, {args})'
+
+
+class NumLoopr(Loopr):
+    CHARSETS = {
+        'ascii': '0123456789',
+        'fullwidth': '０１２３４５６７８９'
+    }
+
+    def __init__(self, upperbound, start=0, by='AB', charset='ascii', places=0, **kwargs):
+        val = list(range(start, upperbound + 1))
+        if charset:
+            cs = self.get_charset(charset)
+            val = self.numbers(start, upperbound, places, cs)
+        return super().__init__(val=val, by=by, **kwargs)
+
+    @classmethod
+    def numbers(cls, start, upper_bound_inclusive, places, charset) -> list:
+        """Only works for base10"""
+        val = []
+        # bind globals
+        int_, str_ = int, str
+        charset_len = len(charset)
+        for n in range(start, upper_bound_inclusive + 1):
+            num = ('{:0%d}' % places).format(n)
+            mapped = ''.join(charset[int_(d)] for d in num)
+            val.append(mapped)
+        return val
+
+    @classmethod
+    def get_charset(cls, charset):
+        cs = cls.CHARSETS.get(charset)
+        if cs:
+            return cs
+        raise NotImplementedError(f'unsupported charset: {charset}')
 
 class LazyAccessDict:
     """Maps dotted access to dict key, returns fallback value on absent keys"""
@@ -55,16 +182,34 @@ class LazyAccessDict:
 
 
 
-def format_list(li):
-    joined = ', '.join(str(x) for x in li)
-    return f'[ {joined} ]'
+def format_columns(rendered):
+    ncols = max(len(row) for row in rendered)
+    longest_per_col = [0] * ncols
+    for row in rendered:
+        for n, token in enumerate(row):
+            length = len(token) if token else 0
+            if length > longest_per_col[n]:
+                longest_per_col[n] = length
+
+    formatted = []
+    for row in rendered:
+        line = ' - '.join(
+            ('{:%d}' % width).format(token or '')
+            for token, width in zip_longest(row, longest_per_col, fillvalue='')
+        )
+        formatted.append(line)
+    return formatted
 
 
-def get_frame_range(string):
-    found = re.search(r"""(\d+)~(\d+)([@\^])?$""", string)
+def get_frame_range(string, charset='ascii'):
+    digits = r'\d'
+    if charset:
+        cs = ''.join(NumLoopr.get_charset(charset))
+        digits = f'[{cs}]'
+    found = re.search(r"""(%s+)~(%s+)([@\^\>])?$""" % (digits, digits), string)
     if not found:
         raise ParseError(f'bad syntax: expected format "name123~456" with '
-                         f'optional trailing ^ or @, got "{string}"')
+                         f'optional trailing ^ or @ or >, got "{string}"')
 
     starts, ends, rep = found.groups()
 
@@ -72,16 +217,17 @@ def get_frame_range(string):
     stub = string[:found.start()]
 
     repeat = {
-        '^': RepeatBy.PENDULUM,
-        '@': RepeatBy.LOOP,
+        '^': 'ABA',
+        '@': 'AB',
+        '>': 'ABB',
     }.get(rep, None)
 
     return stub, pad, int(starts), int(ends), repeat
 
 
 def infinite_generator(value=None):
-    while 1:
-        yield value
+    yield from Loopr(value, by='A')
+
 
 
 class CueSheet:
@@ -97,42 +243,32 @@ class CueSheet:
         opt = {}
         for key, typecast in OPTION_TYPES.items():
             if key in options:
-                opt[key] = typecast(options[key])
+                value = options[key]
+                if typecast is RgbTuple:
+                    opt[key] = tuple(int(n) for n in value.split(','))
+                else:
+                    opt[key] = typecast(value)
         return LazyAccessDict(opt)
 
 
     @classmethod
-    def generate_frames(cls, frame_stub, pad, start, end, repeat=None):
+    def generate_frames(cls, frame_stub, pad, start, end, charset=None, repeat=None):
         idx = start
-        template = '{}{:0%d}' % pad
-        frame = template.format(frame_stub, idx)
+        template = '{}{}'
+        charset = charset or 'ascii'
 
-        step = 1
-        while True:
-            yield frame
-            idx += step
-            frame = template.format(frame_stub, idx)
-
-            if repeat == RepeatBy.LOOP:
-                if idx > end:
-                    idx = start
-                    frame = template.format(frame_stub, idx)
-
-            elif repeat == RepeatBy.PENDULUM:
-                if idx == start or idx == end:
-                    step = -step
-
-            elif idx > end:
-                break
+        loop = NumLoopr(end, by=repeat or 'AB', start=start, charset=charset, places=pad)
+        for idx in loop:
+            yield template.format(frame_stub, idx)
 
         yield from infinite_generator(None)
 
 
     @classmethod
-    def layer_to_generators(cls, layertext):
+    def layer_to_generators(cls, layertext, charset=None):
         try:
-            stub, pad, start, end, rep = get_frame_range(layertext)
-            return cls.generate_frames(stub, pad, start, end, rep)
+            stub, pad, start, end, rep = get_frame_range(layertext, charset)
+            return cls.generate_frames(stub, pad, start, end, charset, rep)
         except ParseError:
             return infinite_generator(layertext)
 
@@ -150,7 +286,7 @@ class CueSheet:
                 for j, layertext in enumerate(layers):
                     if not layertext:
                         continue
-                    gen = self.layer_to_generators(layertext)
+                    gen = self.layer_to_generators(layertext, self.opt.charset)
                     score[j] = gen
 
             # Yield for this iteration, then increment
@@ -159,6 +295,9 @@ class CueSheet:
 
 
     def open_img(self, imgfile, ext='.png'):
+        if not imgfile:
+            return None
+
         if not imgfile.endswith(ext):
             imgfile += ext
 
@@ -169,27 +308,41 @@ class CueSheet:
 
         scale = self.opt.rescale
         if scale:
-            w, h = img.size
-            img = img.resize((w * scale, h * scale))
+            w, h = [int(dim * scale) for dim in img.size]
+            img = img.resize((w, h))
 
         return img
 
 
-    def knit(self, filename, *layers):
-        base = self.open_img(layers[0])
+    def pad_img_to(self, img, width, height):
+        bgcol = (0, 0, 0, 255 if OUT_FORMAT == '.webm' else 0)
+        x, y = 0, 0
+        align = self.opt.align or 'c'
+        new = Image.new('RGBA', (width, height), color=bgcol)
+        if align == 's':
+            x = width // 2 - (img.width // 2)
+            y = height - img.height
+        elif align == 'c':
+            x = width // 2 - (img.width // 2)
+            y = height // 2 - (img.height // 2)
+        elif align == 'w':
+            x = 0
+            y = height // 2 - (img.height // 2)
+        new.paste(img, box=(x, y), mask=img)
+        return new
 
-        for lyr in layers[1:]:
-            if not lyr:
-                continue
 
-            img = self.open_img(lyr)
-            if not img:
-                continue
+    def knit(self, *layers, pad_dimensions=None):
+        images = list(filter(None, [self.open_img(lyr) for lyr in layers]))
+        if pad_dimensions:
+            images = [self.pad_img_to(img, *pad_dimensions) for img in images]
+        if self.opt.bgcol:
+            dim = images[0].size
+            images.insert(0, Image.new('RGBA', dim, color=self.opt.bgcol))
+        base = images[0]
 
+        for img in images[1:]:
             base.alpha_composite(img)
-
-        if filename:
-            base.save(filename)
 
         return base
 
@@ -198,21 +351,39 @@ class CueSheet:
         fmt = 'temp/%05d.png'
 
         if exists('temp'):
-            shutil.rmtree('temp')
-        os.mkdir('temp')
+            for file in glob('temp/*'):
+                os.remove(file)
+        else:
+            os.mkdir('temp')
 
         frames = []
+        filenames = []
+        rendered = list(self.render())
+        formatted = format_columns(rendered)
 
-        for i, layers in enumerate(self.render()):
+        pad = []
+        if self.opt.align:
+            pad = [0, 0]
+            for layers in rendered:
+                images = list(filter(None, [self.open_img(lyr) for lyr in layers]))
+                w = max(img.width for img in images) if images else 0
+                h = max(img.height for img in images) if images else 0
+                if w > pad[0]:
+                    pad[0] = w
+                if h > pad[1]:
+                    pad[1] = h
+
+        for i, layers in enumerate(rendered):
             if VERBOSITY > 0:
-                print(f'{i:>4} ', format_list(layers))
+                print(f'{i:>3} | {formatted[i]} | {i:>3}')
 
-            filename = None  # If truthy, write frames to disk
+            frame = self.knit(*layers, pad_dimensions=pad)
+            frames.append(frame)
+
+        for i, frame in enumerate(frames):
             if force_write_frames or outname.endswith('.webm'):
                 filename = fmt % i
-
-            frame = self.knit(filename, *layers)
-            frames.append(frame)
+                frame.save(filename)
 
         if outname.endswith('.webm'):
             self.make_webm(fmt, fps, outname)
@@ -383,4 +554,3 @@ if __name__ == '__main__':
     p = Parser(text)
     for i, cs in enumerate(p.parse()):
         cs.compose(f'{i + 1:>03}{OUT_FORMAT}')
-
