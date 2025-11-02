24dde290433773e850f0f9186dcbab4eb71b8387
fiffu <fhejehfif@gmail.com>
2025-11-02 14:15:38 +0800

    Support png_ extension out-of-the-box and refactor for neatness

--- mvdecrypt.py
+++ mvdecrypt.py
@@ -1,314 +1,231 @@
 from argparse import ArgumentParser
-import json
-from glob import glob
-import os
 from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass
+from datetime import datetime
+from functools import partial
+import json
+from pathlib import Path
+import sys
+from typing import Generator
 
-# 2k-specific
-import io
-import struct
-import zlib
+def progressbar(iterable, length=None):
+    """Prints progressbar using click. Noop if click isn't installed."""
+    try:
+        import click
+        return click.progressbar(iterable, length=length)
+    except ImportError:
+        from contextlib import nullcontext
+        return nullcontext(iterable)
 
-from PIL import Image
-
-
-
-abspath = os.path.abspath
-
-
-def parse_error(e):
+def format_error(e: BaseException):
     ecls = e.__class__.__name__
     emsg = str(e)
     return f'{ecls}: {emsg}'
 
+def sizeof_fmt(num, suffix='B'):
+    for unit in ('', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi'):
+        if abs(num) < 1024.0:
+            return f'{num:3.1f} {unit}{suffix}'
+        num /= 1024.0
+    return f'{num:.1f} Yi{suffix}'
 
-def ngrams(it, chunk_size):
-    """Yields n-ples from iterable where n is chunk_size"""
-    for i in range(0, len(it), chunk_size):
-        yield it[i:i+chunk_size]
-
-
-def traverse(root_dir):
-    join = os.path.join
-
-    def walk(path):
-        for (root, dirs, files) in os.walk(path):
-            yield (root, dirs, files)
-
-    for path in walk(root_dir):
-        yield path
+def exit(message):
+    print(message)
+    sys.exit(0)
 
 
-def select_files(relpath, select_extensions):
-    join = os.path.join
-    print(relpath)
+@dataclass
+class Args:
+    root_path: Path
+    output_folder: Path
 
-    for curdir, subdirs, files in traverse(relpath):
-        for file in files:
-            for ext in select_extensions:
-                if not file.endswith(ext):
-                    continue
-                path = join(curdir, file)
-                yield (path, ext)
-                break
+    @classmethod
+    def parse(cls):
+        parser = ArgumentParser()
 
+        parser.add_argument('-r', '--root',
+                            type=Path,
+                            default='.',
+                            metavar='PATH',
+                            help='Root directory; default: current directory')
+        parser.add_argument('-o', '--out',
+                            type=str,
+                            default='./decrypted_pictures',
+                            metavar='PATH',
+                            help='Path relative to root directory where decrypted pictures '
+                                 'are written to; default: ./decrypted_pictures')
 
-def find_key(root_dir, target_file='System.json'):
-    config = None
-    _, ext = os.path.splitext(target_file)
+        args = parser.parse_args()
 
-    for relpath, _ in select_files(root_dir, [ext]):
-        if relpath.endswith(target_file):
-            config = os.path.join(root_dir, relpath)
-            break
-
-    else:
-        raise FileNotFoundError(f'Could not find {target_file}')
-
-    try:
-        with open(config, 'r', encoding='utf-8') as f:
-            return json.load(f).get('encryptionKey')
-
-    except BaseException as e:
-        raise RuntimeError(f'Found {target_file} but failed to read '
-                           f'encryption key ({parse_error(e)})')
-
-
-def mkdir(path):
-    exists, split = os.path.exists, os.path.split
-
-    breadcrumbs = []
-    head, file = split(path)
-
-    while head and (not exists(head)):
-        breadcrumbs.append(head)
-        head, _ = split(head)
-
-    # First path in the breadcrumbs is deepest in folder tree
-    # Iterate in reverse to build from shallowest folder first
-    for head in breadcrumbs[::-1]:
-        if not exists(head):
-            os.mkdir(head)
-
-
-
-def write(path, data, flags='wb', retry=True):
-    try:
-        with open(path, flags) as f:
-            f.write(data)
-    except FileNotFoundError:
-        if not retry:
-            raise
-
-        mkdir(path)
-        write(path, data, flags, retry=False)
-
-
-
-class DecryptError(ValueError):
-    pass
-
+        return cls(
+            root_path=args.root,
+            output_folder=args.root / Path(args.out)
+        )
 
 
 class Decryptor:
-    header_len = 16
-    signature = "5250474d56000000"
-    version = "000301"
-    remain = "0000000000"
+    HEADER_LENGTH = 16
+    MAGIC_SIGNATURE = "5250474d56000000"
+    MAGIC_VERSION = "000301"
+    MAGIC_PADDING = "0000000000"
 
-    def __init__(self, root_dir, key):
-        self.root = root_dir
-        self.key = key
+    MAGIC_HEADER = MAGIC_SIGNATURE + MAGIC_VERSION + MAGIC_PADDING
 
-    @classmethod
-    def make_fake_header(cls, self):
-        header = cls.signature + cls.version + cls.remain
-        return [int(x, 16) for x in ngrams(header, 2)]
+    PNG_EXTENSION = '.png'
 
+    def __init__(self, root: Path):
+        self.root = root
+        self.key = None
 
     @property
-    def key_array(self):
-        b16int = lambda n: int(n, 16)
-        return bytearray(map(b16int, ngrams(self.key, 2)))
+    def pictures_path(self):
+        return self.root.joinpath(*self.PICTURES_PATH)
 
+    @property
+    def system_json_path(self):
+        return self.root.joinpath(*self.SYSTEM_JSON_PATH)
 
-    def decrypt(self, crypt, ignore_fake_header=True):
-        hlen = self.header_len
+    @property
+    def extension(self):
+        return self.EXTENSION
 
-        if not ignore_fake_header:
-            header = byteArray[:hlen]
-            if not self.make_fake_header() == header:
-                raise DecryptError("Input file doesn't seem to have fake-header. "
-                                   'Ensure that target file is actually encrypted '
-                                   'of try again with ignore_fake_header=True')
+    @property
+    def output_extension(self):
+        return self.PNG_EXTENSION
 
-        # Trim "fake" header
-        crypt = crypt[hlen:]
+    @property
+    def encryption_key(self):
+        if self.key:
+            return self.key
+
+        self.key = self._load_key(self.system_json_path)
+        return self.key
+
+    def generate_output_path(self, path: Path):
+        pictures_stem = str(self.pictures_path)
+        p = str(path)
+        p = p.removeprefix(pictures_stem)
+        p = p.removesuffix(self.extension)
+        p = '.' + p + self.output_extension
+        return Path(p)
+
+    def valid(self) -> bool:
+        if self.encryption_key:
+            for _ in self.walk_pictures():
+                return True
+        return False
+
+    def decrypt(self, path: Path) -> bytearray:
+        crypt = self._read_bytearray(path)
+
+        # Trim "fake" header bytes
+        crypt = crypt[self.HEADER_LENGTH:]
 
         # XOR key with real header
-        header = crypt[:hlen]
-        for i, (h, k) in enumerate(zip(header, self.key_array)):
+        real_header = crypt[:self.HEADER_LENGTH]
+
+        for i, (h, k) in enumerate(zip(real_header, self.encryption_key)):
             crypt[i] = h ^ k
 
         return crypt
 
+    def walk_pictures(self, path=None) -> Generator[Path, None, None]:
+        for subpath in (path or self.pictures_path).iterdir():
+            if subpath.is_file() and subpath.suffix == self.extension:
+                yield subpath
 
-    def decrypt_file(self, relpath, outdir, from_ext, to_ext, trim_prefix=None):
-        outfile = relpath.replace(from_ext, to_ext)
-        if trim_prefix:
-            outfile = outfile.replace(trim_prefix, '')
-            if outfile[0] in '/\\':
-                outfile = outfile[1:]
-        outpath = os.path.join(outdir, outfile)
+            if subpath.is_dir():
+                yield from self.walk_pictures(subpath)
 
-        with open(relpath, 'rb') as f:
-            clear = f.read()
-            if self.key:
-                clear = self.decrypt(bytearray(clear))
+    def _read_bytearray(self, path: Path) -> bytearray:
+        with open(path, 'rb') as file:
+            content = file.read()
+            return bytearray(content)
 
-        write(outpath, clear)
+    def _load_key(self, path: Path) -> bytearray:
+        try:
+            contents = path.read_text(encoding='utf8')
+            contents_json = json.loads(contents)
 
+            key: str = contents_json['encryptionKey']
+            key_pairwise = [key[i:i+2] for i in range(0, len(key), 2)]
 
-
-class Decryptor2k(Decryptor):
-    MAGIC = b'XYZ1'
-    PALETTE_SIZE = 0xFF
-
-    @staticmethod
-    def cleave(indexable, index):
-        return indexable[:index], indexable[index:]
-
-
-    def decrypt(self, crypt):
-        unpack = struct.unpack
-
-        header, body = self.cleave(crypt, 8)
-        magic, meta = self.cleave(header, 4)
-
-        if magic != self.MAGIC:
-            raise DecryptError(
-                f'Missing 2k header, expected "{self.MAGIC}", got  "{magic}"')
-
-        body = io.BytesIO(zlib.decompress(body))
-
-        palette = []
-        for _ in range(self.PALETTE_SIZE + 1):
-            r, g, b = unpack('=3B', body.read(3))
-            palette.append((r, g, b))
-
-        width, height = unpack('=HH', meta)
-
-        img = Image.new('RGBA', (width, height))
-        img_pixels = img.load()
-        for y in range(height):
-            for x in range(width):
-                r, g, b = palette[ord(body.read(1))]
-                a = 0 if b == self.PALETTE_SIZE else 0xFF
-                img_pixels[x, y] = (r, g, b, a)
-
-        buf = io.BytesIO()
-        img.save(buf, 'PNG')
-        buf.seek(0)
-        return buf.read()
+            parse_base16 = lambda n: int(n, 16)
+            return bytearray(parse_base16(pair) for pair in key_pairwise)
+        except FileNotFoundError:
+            return None
+        except KeyError:
+            exit("Empty encryption key in game data. Most likely it's not encrypted!")
+        except BaseException as e:
+            raise RuntimeError(f'Found {path} but failed to read encryption key ({format_error(e)})')
 
 
-def get_args():
-    parser = ArgumentParser()
-
-    parser.add_argument('-k', dest='key',
-                        type=str,
-                        metavar='KEY',
-                        help='Specify key to use')
-
-    parser.add_argument('-u', dest='unencrypted',
-                        action='store_true',
-                        help='Indicates unencrypted assets (aliases `-k ""`)')
-
-    parser.add_argument('-x', dest='extension',
-                        type=str,
-                        default=['rpgmvp:png', 'png:png', 'png_:png'],
-                        action='append',
-                        metavar='FROM:TO',
-                        help='Extension mapping: pass this once for each '
-                             'mapping; defaults to "-x rpgmvp:png -x png:png -x png_:png"')
-
-    parser.add_argument('-2', '--rm2k',
-                        action='store_true',
-                        help='Indicates RPGM2k files; alias for '
-                             '"-u -x xyz:png -i Picture"')
-
-    parser.add_argument('-r', '--root',
-                        type=abspath,
-                        default='.',
-                        metavar='PATH',
-                        help='Root directory; defaults to current directory')
-
-    parser.add_argument('-i', '--in', dest='indir',
-                        metavar='PATH',
-                        help='Relative path to directory storing encrypted '
-                             'files; defaults to "www/img/pictures"')
-
-    parser.add_argument('-o', '--out', dest='outdir',
-                        type=os.path.normpath,
-                        default=None,
-                        metavar='PATH',
-                        help='Relative path to directory to store decrypted '
-                             'output; leave blank to infer from input_dir')
-
-    args = parser.parse_args()
-
-    if not args.indir:
-        args.indir = os.path.normpath(
-            'Picture' if args.rm2k else 'www/img/pictures')
-
-    if not args.outdir:
-        args.outdir = os.path.split(args.indir)[-1]
-
-    if args.outdir == args.indir:
-        args.outdir += '_decrypted'
-
-    if args.rm2k:
-        args.unencrypted = False
-        args.key = True
-        args.extension.append('xyz:png')
-    return args
+class RMMV(Decryptor):
+    PICTURES_PATH = 'www/img/pictures'.split('/')
+    SYSTEM_JSON_PATH = 'www/data/System.json'.split('/')
+    EXTENSION = '.rpgmvp'
 
 
-def main():
-    args = get_args()
-    original_dir = os.getcwd()
+class RMMV2(Decryptor):
+    PICTURES_PATH = 'img/pictures'.split('/')
+    SYSTEM_JSON_PATH = 'data/System.json'.split('/')
+    EXTENSION = '.png_'
 
-    outdir = args.outdir
-    indir = args.indir
-    root = args.root
 
-    exts = {}
-    for pair in args.extension:
-        xfrom, xto = pair.split(':')
-        exts[xfrom] = xto
+class Main:
+    VERSIONS = [RMMV, RMMV2]
 
-    try:
-        os.chdir(root)
+    def __init__(self, args: Args):
+        self.args = args
 
-        key = '' if args.unencrypted else args.key
-        if key is None:
-            key = find_key(root)
-            if not key:
-                with open('__unencrypted', 'w'):
-                    return
+    def call(self):
+        decryptor = self.detect(self.args.root_path)
+        if not decryptor:
+            raise RuntimeError('cannot detect game version')
 
-        dec = Decryptor(root, key)
-        dec2k = Decryptor2k(root, key)
+        if not self.args.output_folder.exists():
+            self.args.output_folder.mkdir(parents=True)
 
-        with ThreadPoolExecutor(max_workers=8) as pool:
-            for file, ext in select_files(indir, list(exts.keys())):
-                d = dec2k if ext == 'xyz' else dec
-                pool.submit(d.decrypt_file, file, outdir, ext, exts[ext], indir)
-                # d.decrypt_file(file, outdir, ext, exts[ext], indir)
+        inputs = list(decryptor.walk_pictures())
+        bytes_written = self.decrypt_all(decryptor, inputs)
 
-    finally:
-        os.chdir(original_dir)
+        return len(inputs), bytes_written
+
+    def decrypt_all(self, decryptor, inputs):
+        written = 0
+
+        with ThreadPoolExecutor() as pool:
+            futures = pool.map(partial(self.decrypt_one, decryptor), inputs)
+
+            with progressbar(futures, length=len(inputs)) as bar:
+                for file_size in bar:
+                    written += file_size
+
+        return written
+
+    def detect(self, root_path: Path) -> Decryptor:
+        for version in self.VERSIONS:
+            if version(root_path).valid():
+                return version(root_path)
+
+    def decrypt_one(self, decryptor: Decryptor, input_file: Path) -> int:
+        output_relpath = decryptor.generate_output_path(input_file)
+
+        output_path = self.args.output_folder / output_relpath
+        if not output_path.parent.exists():
+            output_path.parent.mkdir(parents=True)
+
+        clear = decryptor.decrypt(input_file)
+        return output_path.write_bytes(clear)
 
 
 if __name__ == '__main__':
-    main()
+    args = Args.parse()
+
+    started = datetime.now()
+    files_written, bytes_written = Main(args).call()
+    elapsed = datetime.now() - started
+
+    bytes_written = sizeof_fmt(bytes_written)
+    print(f'Decrypted {files_written} files totalling {bytes_written} in {elapsed.seconds} secs')
+
